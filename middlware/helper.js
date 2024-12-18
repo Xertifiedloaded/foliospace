999c4b102189902c6e9b6e421eeb88f26ce96936
Xertifiedloaded <certifiedloaded@yahoo.com>
2024-12-18 06:57:08 +0100

    stc

--- middlware/helper.js
+++ middlware/helper.js
@@ -38,7 +38,7 @@ export const generateToken = (user) => {
 
 
 export function cookiesSerialize(token) {
-  const isProduction = process.env.NODE_ENV === "production";
+  const isProduction = process.env.NODE_ENV ;
 
   return serialize("token", token, {
     httpOnly: true,
