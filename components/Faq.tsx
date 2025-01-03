bedd363e6a3dd0cbbffec89e3b6d99e71982a095
Xertifiedloaded <certifiedloaded@yahoo.com>
2025-01-03 13:37:27 +0100

    faq

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -11,9 +11,9 @@ import { faqData } from "@/utils/data";
   
   export function FAQ() {
     return (
-      <section className="w-full wrapper py-12 px-4">
+      <section className="w-full max-w-5xl mx-auto py-12 px-4">
         <div className="text-center mb-10">
-          <h2 className="text-2xl font-bold tracking-tight mb-4">
+          <h2 className="lg:text-4xl text-2xl font-bold tracking-tight mb-4">
             Frequently Asked Questions
           </h2>
           <p className="text-muted-foreground">
